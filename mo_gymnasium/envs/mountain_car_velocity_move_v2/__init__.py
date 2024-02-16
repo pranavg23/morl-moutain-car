5b05d244dabb3098eff683bb4e36b33660881652
Pranav Gupta <praanavgupta@gmail.com>
2024-02-16 16:56:21 +0000

    Fixing v2 - try 2

--- mo_gymnasium/envs/mountain_car_velocity_move_v2/__init__.py
+++
@@ -1,8 +0,0 @@
-from gymnasium.envs.registration import register
-
-
-register(
-    id="mo-mountaincar-velocity-move-v2",
-    entry_point="mo_gymnasium.envs.mountain_car_move_velocity_move_v2.mountain_car_velocity_move:MOMountainCar",
-    max_episode_steps=200,
-)
