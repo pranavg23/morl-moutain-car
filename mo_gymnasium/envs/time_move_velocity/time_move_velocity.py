710b170cd0f8238daba1ba109e8ff6adbfb89af7
Pranav Gupta <praanavgupta@gmail.com>
2024-03-03 17:34:47 +0000

    Testing

--- mo_gymnasium/envs/time_move_velocity/time_move_velocity.py
+++ mo_gymnasium/envs/time_move_velocity/time_move_velocity.py
@@ -23,11 +23,11 @@ class MOMountainCar(MountainCarEnv, EzPickle):
     def __init__(self, render_mode: Optional[str] = None, goal_velocity=0):
         super().__init__(render_mode, goal_velocity)
         EzPickle.__init__(self, render_mode, goal_velocity)
+        self.reward_space = spaces.Box(low=np.array([-1, -1]), high=np.array([-1, 0, 0]), shape=(3,), dtype=np.float32)
+        self.reward_dim = 3
 
         # self.reward_space = spaces.Box(low=np.array([-1, -1, 0]), high=np.array([-1, 0, math.inf]), shape=(3,), dtype=np.float32)
         # self.reward_dim = 3
-        self.reward_space = spaces.Box(low=np.array([-1, -1]), high=np.array([-1, 0, 0]), shape=(3,), dtype=np.float32)
-        self.reward_dim = 3
         print(self.reward_space)
 
     def step(self, action: int):
