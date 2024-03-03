4d42ce5a58dae314bd591c3330f74b8dad2a2431
Pranav Gupta <praanavgupta@gmail.com>
2024-03-03 17:57:01 +0000

    New

--- mo_gymnasium/envs/time_move_velocity/__init__.py
+++ mo_gymnasium/envs/time_move_velocity/__init__.py
@@ -2,7 +2,7 @@ from gymnasium.envs.registration import register
 
 
 register(
-    id="time_move_velocity",
-    entry_point="mo_gymnasium.envs.time_move_velocity.time_move_velocity:MOMountainCar",
+    id="mo-mountaincar-v0",
+    entry_point="mo_gymnasium.envs.mountain_car.mountain_car:MOMountainCar",
     max_episode_steps=200,
 )
--- mo_gymnasium/envs/time_move_velocity/time_move_velocity.py
+++ mo_gymnasium/envs/time_move_velocity/time_move_velocity.py
@@ -24,16 +24,15 @@ class MOMountainCar(MountainCarEnv, EzPickle):
         super().__init__(render_mode, goal_velocity)
         EzPickle.__init__(self, render_mode, goal_velocity)
 
-        self.reward_space = spaces.Box(low=np.array([-1, 0]), high=np.array([0, 1.1]), shape=(2,), dtype=np.float32)
-        self.reward_dim = 2
-        print(self.reward_space)
+        self.reward_space = spaces.Box(low=np.array([-1, -1, -1]), high=np.array([-1, 0, 0]), shape=(3,), dtype=np.float32)
+        self.reward_dim = 3
 
     def step(self, action: int):
         assert self.action_space.contains(action), f"{action!r} ({type(action)}) invalid"
 
         position, velocity = self.state
         velocity += (action - 1) * self.force + math.cos(3 * position) * (-self.gravity)
-        velocity = np.clip(velocity, -self.max_speed, self.max_speed) #Do we need consider changing the clipping?
+        velocity = np.clip(velocity, -self.max_speed, self.max_speed)
         position += velocity
         position = np.clip(position, self.min_position, self.max_position)
         if position == self.min_position and velocity < 0:
@@ -41,9 +40,10 @@ class MOMountainCar(MountainCarEnv, EzPickle):
 
         terminated = bool(position >= self.goal_position and velocity >= self.goal_velocity)
         # reward = -1.0
-        reward = np.zeros(2, dtype=np.float32)
-        reward[0] = 0.0 if action == 1 else -1.0  # movement penalty
-        reward[1] = 15*abs(velocity) #Rewarding magnitude of velocity
+        reward = np.zeros(3, dtype=np.float32)
+        reward[0] = 0.0 if terminated else -1.0  # time penalty
+        reward[1] = 0.0 if action != 0 else -1.0  # reverse penalty
+        reward[2] = 0.0 if action != 2 else -1.0  # forward penalty
 
         self.state = (position, velocity)
         if self.render_mode == "human":
