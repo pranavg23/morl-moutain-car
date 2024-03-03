e86913424b01e4226cd35892812430525fd5641d
Pranav Gupta <praanavgupta@gmail.com>
2024-03-03 18:03:14 +0000

    New

--- mo_gymnasium/envs/mountain_car_3obj/__init__.py
+++ mo_gymnasium/envs/mountain_car_3obj/__init__.py
@@ -3,6 +3,6 @@ from gymnasium.envs.registration import register
 
 register(
     id="mountaincar3obj",
-    entry_point="mo_gymnasium.envs.mountaincar3obj.mountaincar3obj:MOMountainCar",
+    entry_point="mo_gymnasium.envs.mountain_car_3obj.mountain_car_3obj:MOMountainCar",
     max_episode_steps=200,
 )
